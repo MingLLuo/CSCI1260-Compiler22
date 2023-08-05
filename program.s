f3f00c04b3f8899bb459cbc0b38f5ee241ee67ff
MingLLuo <sh770240869@163.com>
2023-08-06 00:27:25 +0800

    sep26

--- program.s
+++ program.s
@@ -1,30 +1,8 @@
 global entry
 entry:
-	mov rax, 16
-	and rax, 3
-	cmp rax, 0
-	mov rax, 0
-	setz al
-	shl rax, 7
-	or rax, 31
-	cmp rax, 31
-	jz else__0
-	mov rax, 31
-	and rax, 3
-	cmp rax, 0
-	mov rax, 0
-	setz al
-	shl rax, 7
-	or rax, 31
-	cmp rax, 31
-	jz else__2
 	mov rax, 4
-	jmp continue__3
-else__2:
-	mov rax, 8
-continue__3:
-	jmp continue__1
-else__0:
-	mov rax, 12
-continue__1:
+	mov [rsp + -8], rax
+	mov rax, 31
+	mov r8, [rsp + -8]
+	add rax, r8
 	ret
