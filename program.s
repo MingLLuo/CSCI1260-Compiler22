61fef8da559765a5a0f113b7f3080f69ad7e6fd7
MingLLuo <sh770240869@163.com>
2023-08-16 22:26:23 +0800

    fix

--- program.s
+++ program.s
@@ -2,9 +2,18 @@ global entry
 extern error
 extern read_num
 entry:
-	mov [rsp + -8], rdi
-	add rsp, -8
-	call read_num
-	sub rsp, -8
-	mov rdi, [rsp + -8]
+	mov rax, 4
+	mov [rsp + -8], rax
+	mov rax, 8
+	mov r8, [rsp + -8]
+	mov [rdi + 0], r8
+	mov [rdi + 8], rax
+	mov rax, rdi
+	or rax, 2
+	add rdi, 16
+	mov r8, rax
+	and r8, 7
+	cmp r8, 2
+	jnz error
+	mov rax, [rax + -2]
 	ret
